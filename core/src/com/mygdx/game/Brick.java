f73d1334bae07cbe3a10d5808ec272d9e2beeeb7
Alex <warlordofspace@gmail.com>
2019-04-19 16:29:59 +0930

    Start

---
+++ core/src/com/mygdx/game/Brick.java
@@ -0,0 +1,13 @@
+package com.mygdx.game;
+
+public class Brick {
+    int posX;
+    int posY;
+    boolean status;
+
+    Brick(int posX, int posY, boolean status){
+        this.posX=posX;
+        this.posY=posY;
+        this.status=status;
+    }
+}
--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -6,15 +6,52 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import static com.badlogic.gdx.graphics.g3d.particles.ParticleChannels.TextureRegion;
 
 public class GameScreen implements Screen {
     private MyGdxGame controller;
-    Texture texture;
+    Texture piecesTexture;
+    Sprite ball;
+    Sprite paddle;
+    Sprite brick;
+    Brick bricks[] = new Brick[20];
     // constructor to keep a reference to the main Game class
     public GameScreen(MyGdxGame game){
 
         this.controller = game;
+        piecesTexture=new Texture("breakout_pieces.png");
+        brick = new Sprite(piecesTexture, 0, 0, 40, 25);
+        int height=Gdx.graphics.getHeight();
+        int width=Gdx.graphics.getWidth();
+
+
+        brick.setPosition(10, 10);
+        controller.batch.begin();
+        int brickY=16;
+        for(int i=0; i<4; i++){
+            int brickX=0;
+            for (int b=0; b<20; b++){
+
+                bricks[bricks.length-1]=new Brick(brickX, brickY, true);
+                brick.draw(controller.batch);
+                brickX+=40;
+
+            }
+            brickY+=25;
+
+        }
+        controller.batch.end();
+        //ball.setRotation(45);
+        /*
+        ball = new Sprite(texture, 20, 20, 50, 50);
+        ball.setPosition(10, 10);
+        ball.setRotation(45);
+        */
+        //TextureRegion(pieces, int x, int y, int width, int height)
+
     }
     public void create() {
         Gdx.app.log("MenuScreen: ","menuScreen create");
@@ -25,6 +62,7 @@ public class GameScreen implements Screen {
        Gdx.gl.glClearColor(1,0,0,1);
        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
        controller.batch.begin();
+
       // controller.batch.draw(texture, 0,0);
        controller.batch.end();
         Gdx.app.log("MenuScreen: ","gameScreen started");
